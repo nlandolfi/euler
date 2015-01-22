2ddec72bd837db1a1d2a6d60a05a83b5b6ff9acc
Nick Landolfi <nclandolfi@gmail.com>
2015-01-21 20:27:52 -0800

    java problem 2

--- java/Main.java
+++ java/Main.java
@@ -4,5 +4,6 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Project Euler Java Solutions - Nick Landolfi");
         System.out.println(" * Problem 1: " + problems.Problem001.Solution());
+        System.out.println(" * Problem 2: " + problems.Problem002.Solution());
     }
 }
